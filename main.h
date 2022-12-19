038165591a4800dbfe5b67a4145ad3ccd62451df
0xAckerMan <joelkores1@gmail.com>
2022-12-19 18:06:20 +0300

    initial commit

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+
+#endif
